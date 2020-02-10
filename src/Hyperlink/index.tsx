0f6e634830fd518de0e67084db88b236ef6df27c
Kepler Sticka-Jones <kepler+git@stickajones.org>
2020-02-09 21:14:33 -0700

    test: address lints

--- src/Hyperlink/index.tsx
+++ src/Hyperlink/index.tsx
@@ -1,11 +1,13 @@
 import styled from "@emotion/styled";
 
 export const Hyperlink = styled.a`
-  text-decoration: none;
   color: var(--starstuff-hyperlink-default-color);
+  text-decoration: none;
+
   box-shadow: inset 0 -1px 0 0 rgba(var(--starstuff-hyperlink-default-color), 0.5);
 
-  :hover {
+  :hover,
+  :focus {
     box-shadow: inset 0 -2px 0 0 rgba(var(--starstuff-hyperlink-default-color), 0.7);
   }
 
